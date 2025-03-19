dece8dac0033aed04d9aa74fb589f38bb856ce73
Evewibe <aleksey.dantist@gmail.com>
2025-03-19 13:24:16 +0600

    feat: http client

---
+++ FirstMy/src/Bot/Services/Users/IUsersService.cs
@@ -0,0 +1,9 @@
+﻿using FirstMy.Bot.Models;
+
+namespace FirstMy.Bot.Services.Users;
+
+public interface IUsersService
+{
+    Task<User> GetUserAsync(long userId);
+    Task<User> CreateUserAsync(User user);
+}
