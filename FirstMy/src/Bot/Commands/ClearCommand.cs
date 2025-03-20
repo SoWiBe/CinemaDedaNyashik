4083f88a0c62c19b6a711f9113d19a3dc51a7091
Evewibe <aleksey.dantist@gmail.com>
2025-03-21 01:27:27 +0600

    feat: add serilog

---
+++ FirstMy/src/Bot/Commands/ClearCommand.cs
@@ -0,0 +1,13 @@
+﻿using FirstMy.Bot.Commands.Core;
+
+namespace FirstMy.Bot.Commands;
+
+public class ClearCommand : TelegramBotCommand
+{
+    protected override async Task CommandLogic()
+    {
+        return;
+    }
+
+    public ClearCommand(string commandName) : base(commandName){}
+}
---
+++ FirstMy/src/Bot/Commands/Core/TelegramBotCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstMy.Bot.Commands.Core;
+
+public abstract class TelegramBotCommand
+{
+    protected string CommandName;
+
+    protected TelegramBotCommand(string commandName)
+    {
+        CommandName = commandName;
+    }
+
+    protected abstract Task CommandLogic();
+}
